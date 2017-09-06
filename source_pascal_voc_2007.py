4a6c3da26085d74ded41a15c980a3e049eb46ab8
Lukasz Janyst <lukasz@jany.st>
2017-09-06 13:33:17 +0200

    Add the Pascal VOC 2007 data source and some utils

---
+++ source_pascal_voc_2007.py
@@ -0,0 +1,165 @@
+#-------------------------------------------------------------------------------
+# Author: Lukasz Janyst <lukasz@jany.st>
+# Date:   30.08.2017
+#-------------------------------------------------------------------------------
+# This file is part of SSD-TensorFlow.
+#
+# SSD-TensorFlow is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# SSD-TensorFlow is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+#-------------------------------------------------------------------------------
+
+import lxml.etree
+import random
+import math
+import cv2
+import os
+
+import numpy as np
+
+from utils import Label, Box, Sample, Size
+from utils import rgb2bgr, abs2prop
+from glob import glob
+from tqdm import tqdm
+
+#-------------------------------------------------------------------------------
+# Labels
+#-------------------------------------------------------------------------------
+label_defs = [
+    Label('aeroplane',   rgb2bgr((0,     0,   0))),
+    Label('bicycle',     rgb2bgr((111,  74,   0))),
+    Label('bird',        rgb2bgr(( 81,   0,  81))),
+    Label('boat',        rgb2bgr((128,  64, 128))),
+    Label('bottle',      rgb2bgr((244,  35, 232))),
+    Label('bus',         rgb2bgr((230, 150, 140))),
+    Label('car',         rgb2bgr(( 70,  70,  70))),
+    Label('cat',         rgb2bgr((102, 102, 156))),
+    Label('chair',       rgb2bgr((190, 153, 153))),
+    Label('cow',         rgb2bgr((150, 120,  90))),
+    Label('diningtable', rgb2bgr((153, 153, 153))),
+    Label('dog',         rgb2bgr((250, 170,  30))),
+    Label('horse',       rgb2bgr((220, 220,   0))),
+    Label('motorbike',   rgb2bgr((107, 142,  35))),
+    Label('person',      rgb2bgr((152, 251, 152))),
+    Label('pottedplant', rgb2bgr(( 70, 130, 180))),
+    Label('sheep',       rgb2bgr((220,  20,  60))),
+    Label('sofa',        rgb2bgr((  0,   0, 142))),
+    Label('train',       rgb2bgr((  0,   0, 230))),
+    Label('tvmonitor',   rgb2bgr((119,  11,  32)))]
+
+#-------------------------------------------------------------------------------
+def build_sample_list(root, dataset_name):
+    """
+    Build a list of samples for the VOC dataset (either trainval or test)
+    """
+    image_root  = root + '/JPEGImages/'
+    annot_root  = root + '/Annotations/'
+    annot_files = glob(annot_root + '/*xml')
+    samples     = []
+
+    #---------------------------------------------------------------------------
+    # Process each annotated sample
+    #---------------------------------------------------------------------------
+    for fn in tqdm(annot_files, desc=dataset_name, unit='samples'):
+        with open(fn, 'r') as f:
+            doc      = lxml.etree.parse(f)
+            filename = image_root+doc.xpath('/annotation/filename')[0].text
+
+            #-------------------------------------------------------------------
+            # Get the file dimensions
+            #-------------------------------------------------------------------
+            if not os.path.exists(filename):
+                continue
+
+            img     = cv2.imread(filename)
+            imgsize = Size(img.shape[1], img.shape[0])
+
+            #-------------------------------------------------------------------
+            # Get boxes for all the objects
+            #-------------------------------------------------------------------
+            boxes    = []
+            objects  = doc.xpath('/annotation/object')
+            for obj in objects:
+                #---------------------------------------------------------------
+                # Skip dificult detections if we have them labelled
+                #---------------------------------------------------------------
+                difficult = obj.xpath('difficult')
+                if difficult:
+                    difficult = int(difficult[0].text)
+                    if difficult:
+                        continue
+
+                #---------------------------------------------------------------
+                # Get the properties of the box and convert them to the
+                # proportional terms
+                #---------------------------------------------------------------
+                label = obj.xpath('name')[0].text
+                xmin  = int(float(obj.xpath('bndbox/xmin')[0].text))
+                xmax  = int(float(obj.xpath('bndbox/xmax')[0].text))
+                ymin  = int(float(obj.xpath('bndbox/ymin')[0].text))
+                ymax  = int(float(obj.xpath('bndbox/ymax')[0].text))
+                center, size = abs2prop(xmin, xmax, ymin, ymax, imgsize)
+                box = Box(label, center, size)
+                boxes.append(box)
+            if not boxes:
+                continue
+            sample = Sample(filename, boxes, imgsize)
+            samples.append(sample)
+
+    return samples
+
+#-------------------------------------------------------------------------------
+class PascalVOC2007Source:
+    #---------------------------------------------------------------------------
+    def __init__(self, vocid='VOC2007'):
+        self.num_classes   = len(label_defs)
+        self.colors        = {l.name: l.color for l in label_defs}
+        self.num_train     = 0
+        self.num_valid     = 0
+        self.num_test      = 0
+        self.train_samples = []
+        self.valid_samples = []
+        self.test_samples  = []
+        self.vocid         = vocid
+
+    #---------------------------------------------------------------------------
+    def load_raw_data(self, data_dir, valid_fraction):
+        """
+        Load the raw data
+        :param data_dir:       the directory where the dataset's file are stored
+        :param valid_fraction: what franction of the dataset should be used
+                               as a validation sample
+        """
+        trainval_root = data_dir + '/trainval/VOCdevkit/'+self.vocid
+        test_root     = data_dir + '/test/VOCdevkit/'+self.vocid
+
+        trainval_samples   = build_sample_list(trainval_root, 'trainval')
+        random.shuffle(trainval_samples)
+        tvlen              = len(trainval_samples)
+        self.test_samples  = build_sample_list(test_root, 'test    ')
+        self.valid_samples = trainval_samples[:int(valid_fraction*tvlen)]
+        self.train_samples = trainval_samples[int(valid_fraction*tvlen):]
+
+        if len(self.train_samples) == 0:
+            raise RuntimeError('No training samples found in ' + data_dir)
+        if len(self.valid_samples) == 0:
+            raise RuntimeError('No validation samples found in ' + data_dir)
+        if len(self.test_samples) == 0:
+            raise RuntimeError('No testing samples found in ' + data_dir)
+
+        self.num_train = len(self.train_samples)
+        self.num_valid = len(self.valid_samples)
+        self.num_test  = len(self.test_samples)
+
+#-------------------------------------------------------------------------------
+def get_source():
+    return PascalVOC2007Source()
---
+++ utils.py
@@ -0,0 +1,105 @@
+#-------------------------------------------------------------------------------
+# Author: Lukasz Janyst <lukasz@jany.st>
+# Date:   29.08.2017
+#-------------------------------------------------------------------------------
+# This file is part of SSD-TensorFlow.
+#
+# SSD-TensorFlow is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# SSD-TensorFlow is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+#-------------------------------------------------------------------------------
+
+import argparse
+import cv2
+
+import tensorflow as tf
+import numpy as np
+
+from collections import namedtuple
+
+#-------------------------------------------------------------------------------
+def initialize_uninitialized_variables(sess):
+    """
+    Only initialize the weights that have not yet been initialized by other
+    means, such as importing a metagraph and a checkpoint. It's useful when
+    extending an existing model.
+    """
+    uninit_vars    = []
+    uninit_tensors = []
+    for var in tf.global_variables():
+        uninit_vars.append(var)
+        uninit_tensors.append(tf.is_variable_initialized(var))
+    uninit_bools = sess.run(uninit_tensors)
+    uninit = zip(uninit_bools, uninit_vars)
+    uninit = [var for init, var in uninit if not init]
+    sess.run(tf.variables_initializer(uninit))
+
+#-------------------------------------------------------------------------------
+def load_data_source(data_source):
+    """
+    Load a data source given it's name
+    """
+    source_module = __import__('source_'+data_source)
+    get_source    = getattr(source_module, 'get_source')
+    return get_source()
+
+#-------------------------------------------------------------------------------
+def rgb2bgr(tpl):
+    """
+    Convert RGB color tuple to BGR
+    """
+    return (tpl[2], tpl[1], tpl[0])
+
+#-------------------------------------------------------------------------------
+Label  = namedtuple('Label',  ['name', 'color'])
+Size   = namedtuple('Size',   ['w', 'h'])
+Point  = namedtuple('Point',  ['x', 'y'])
+Sample = namedtuple('Sample', ['filename', 'boxes', 'imgsize'])
+Box    = namedtuple('Box',    ['label', 'center', 'size'])
+
+#-------------------------------------------------------------------------------
+def str2bool(v):
+    """
+    Convert a string to a boolean
+    """
+    if v.lower() in ('yes', 'true', 't', 'y', '1'):
+        return True
+    elif v.lower() in ('no', 'false', 'f', 'n', '0'):
+        return False
+    else:
+        raise argparse.ArgumentTypeError('Boolean value expected.')
+
+#-------------------------------------------------------------------------------
+def abs2prop(xmin, xmax, ymin, ymax, imgsize):
+    """
+    Convert the absolute min-max box bound to proportional center-width bounds
+    """
+    width   = float(xmax-xmin)
+    height  = float(ymax-ymin)
+    cx      = float(xmin)+width/2
+    cy      = float(ymin)+height/2
+    width  /= imgsize.w
+    height /= imgsize.h
+    cx     /= imgsize.w
+    cy     /= imgsize.h
+    return Point(cx, cy), Size(width, height)
+
+#-------------------------------------------------------------------------------
+def prop2abs(center, size, imgsize):
+    """
+    Convert proportional center-width bounds to absolute min-max bounds
+    """
+    width2  = size.w*imgsize.w/2
+    height2 = size.h*imgsize.h/2
+    cx      = center.x*imgsize.w
+    cy      = center.y*imgsize.h
+    return int(cx-width2), int(cx+width2), int(cy-height2), int(cy+height2)
