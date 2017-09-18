8fae631946dfad6478b5859ee7a426b9cfc0b768
Lukasz Janyst <lukasz@jany.st>
2017-09-18 20:47:52 +0200

    Implement basic data transforms

---
+++ transforms.py
@@ -0,0 +1,114 @@
+#-------------------------------------------------------------------------------
+# Author: Lukasz Janyst <lukasz@jany.st>
+# Date:   18.09.2017
+#-------------------------------------------------------------------------------
+# This file is part of SSD-TensorFlow.
+#
+# SSD-TensorFlow is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# SSD-TensorFlow is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with SSD-Tensorflow.  If not, see <http://www.gnu.org/licenses/>.
+#-------------------------------------------------------------------------------
+
+import cv2
+
+import numpy as np
+
+from ssdutils import get_anchors_for_preset, get_preset_by_name, anchors2array
+from ssdutils import box2array, compute_overlap, compute_location
+from utils import Size
+
+#-------------------------------------------------------------------------------
+class ImageLoaderTransform:
+    #---------------------------------------------------------------------------
+    def transform(self, data, label, gt):
+        return cv2.imread(gt.filename), label, gt
+
+#-------------------------------------------------------------------------------
+def process_overlap(overlap, box, anchor, matches, num_classes, vec):
+    if overlap.idx in matches and matches[overlap.idx] >= overlap.score:
+        return
+
+    matches[overlap.idx] = overlap.score
+    vec[overlap.idx, 0:num_classes+1] = 0
+    vec[overlap.idx, box.labelid]     = 1
+    vec[overlap.idx, num_classes+1:]  = compute_location(box, anchor)
+
+#-------------------------------------------------------------------------------
+class LabelCreatorTransform:
+    #---------------------------------------------------------------------------
+    def __init__(self, preset, num_classes):
+        self.preset = preset
+        self.num_classes = num_classes
+        self.initialized = False
+
+    #---------------------------------------------------------------------------
+    def initialize(self):
+        self.anchors = get_anchors_for_preset(self.preset)
+        self.vheight = len(self.anchors)
+        self.vwidth = self.num_classes+5 # background class + location offsets
+        self.img_size = Size(1000, 1000)
+        self.anchors_arr = anchors2array(self.anchors, self.img_size)
+        self.initialized = True
+
+    #---------------------------------------------------------------------------
+    def transform(self, data, label, gt):
+        #-----------------------------------------------------------------------
+        # Initialize the data vector and other variables
+        #-----------------------------------------------------------------------
+        if not self.initialized:
+            self.initialize()
+
+        vec = np.zeros((self.vheight, self.vwidth), dtype=np.float32)
+
+        #-----------------------------------------------------------------------
+        # For every box compute the best match and all the matches above 0.5
+        # Jaccard overlap
+        #-----------------------------------------------------------------------
+        overlaps = {}
+        for box in gt.boxes:
+            box_arr = box2array(box, self.img_size)
+            overlaps[box] = compute_overlap(box_arr, self.anchors_arr, 0.5)
+
+        #-----------------------------------------------------------------------
+        # Set up the training vector resolving conflicts in favor of a better
+        # match
+        #-----------------------------------------------------------------------
+        vec[:, self.num_classes]   = 1 # background class
+        vec[:, self.num_classes+1] = 0 # x offset
+        vec[:, self.num_classes+2] = 0 # y offset
+        vec[:, self.num_classes+3] = 0 # log width scale
+        vec[:, self.num_classes+4] = 0 # log height scale
+
+        matches = {}
+        for box in gt.boxes:
+            for overlap in overlaps[box].good:
+                anchor = self.anchors[overlap.idx]
+                process_overlap(overlap, box, anchor, matches, self.num_classes, vec)
+
+        matches = {}
+        for box in gt.boxes:
+            overlap = overlaps[box].best
+            anchor  = self.anchors[overlap.idx]
+            process_overlap(overlap, box, anchor, matches, self.num_classes, vec)
+
+        return data, vec, gt
+
+#-------------------------------------------------------------------------------
+class ResizeTransform:
+    #---------------------------------------------------------------------------
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    #---------------------------------------------------------------------------
+    def transform(self, data, label, gt):
+        return cv2.resize(data, (self.width, self.height)), label, gt
