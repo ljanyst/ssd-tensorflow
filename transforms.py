a8979081ebdfda164dba6987ab5bc45ec6c050fb
Lukasz Janyst <lukasz@jany.st>
2018-03-05 23:44:05 +0100

    Transforms: Wrap the hue value instead of cutting it

--- transforms.py
+++ transforms.py
@@ -177,8 +177,8 @@ class HueTransform(Transform):
         data = data.astype(np.float32)
         delta = random.randint(-self.delta, self.delta)
         data[0] += delta
-        data[0][data[0]>180] = 180
-        data[0][data[0]<0] = 0
+        data[0][data[0]>180] -= 180
+        data[0][data[0]<0] +=180
         data = data.astype(np.uint8)
         data = cv2.cvtColor(data, cv2.COLOR_HSV2BGR)
         return data, label, gt
